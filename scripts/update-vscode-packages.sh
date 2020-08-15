9563bfef71e3e216ac79ed98f69eb57f721ec46a
CMHJ <55470933+CMHJ@users.noreply.github.com>
2020-08-15 14:42:26 +1000

    Fixed pathing to be more absolute

--- scripts/update-vscode-packages.sh
+++ scripts/update-vscode-packages.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
-install_sh="install-vscode-packages.sh"
-current_dir=$(dirname $0)
-echo "#!/bin/sh\n" > $install_sh
-code --list-extensions | xargs -L 1 echo code --install-extension >> $install_sh
+current_dir="$(dirname "$(readlink -f "$0")")"
+install_sh="$current_dir/install-vscode-packages"
+printf "#!/bin/sh\n" > "$install_sh"
+code --list-extensions | xargs -L 1 echo code --install-extension >> "$install_sh"
