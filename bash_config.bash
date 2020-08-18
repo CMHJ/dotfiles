c20f57982a569acb0750bb09e5c9ca0b8b37dece
CMHJ <55470933+CMHJ@users.noreply.github.com>
2020-08-19 08:49:56 +1000

    Added optimised clear command, add descriptions

--- bash_config.bash
+++ bash_config.bash
@@ -18,9 +18,14 @@ alias uur='sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y'
 alias gfs='git fetch && git status'
 alias steal='git clone'
 
-alias pie='perl -p -i -e'
+alias pie='perl -p -i -e' # Useful for running substitute commands on files in dir i.e.
+# pie 's/replace-text/with-this-text/g' ./*.txt
 
 # Custom functions
 cs() { cd "$@" && ls -laF; }
 mkcd() { mkdir -p "$@" && cd "$@"; }
-vic() { vim $(which "$1"); }
+vic() { vim $(which "$1"); } # Useful for editing scripts on path variable
+
+export clear="[3J[H[2J" # Optimised clear function, clears the screen 5 times faster
+clear() { echo -n $clear; }
+
