39f0b438130254b3749157cfc226dff31168e859
CMHJ <55470933+CMHJ@users.noreply.github.com>
2022-01-20 11:32:40 +1100

    Updated bash config

--- bash_config.bash
+++ bash_config.bash
@@ -5,13 +5,20 @@
 # Set vi mode for bash
 set -o vi
 
-# Set script dir in PATH variable
+# Set env vars
+export EDITOR="nvim"
+export LC_ALL="en_US.UTF-8"
+
+# XDG vars
+export XDG_CONFIG_HOME="$HOME/.config"
+
+# NPM vars
+export NPM_CONFIG_PREFIX="$XDG_CONFIG_HOME/npm-global"
+
+# Add script dir in PATH variable
 export PATH="$(dirname "$(readlink -f "$HOME/.bash_aliases")")/scripts":$PATH
 export PATH="$PATH:$HOME/.local/bin"
 
-# Set env vars
-export EDITOR="vim"
-export LC_ALL="en_US.UTF-8"
 
 # Set go paths
 if command -v /usr/bin/go &> /dev/null
@@ -22,18 +29,17 @@ fi
 
 # Custom Aliases
 #unalias -a
-#alias v='nvim'
-alias v='vim'
 alias c='clear'
-alias uu='sudo apt update && sudo apt upgrade -y'
-alias uur='sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y'
+alias auu='sudo apt update && sudo apt upgrade -y'
+alias auur='sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y'
 alias gs='git status'
 alias gfs='git fetch && git status'
-alias gs='git status'
+alias gfp='git fetch --prune'
 alias gd='git diff'
 alias steal='git clone'
 alias copy='xclip -sel clip'
 alias paste='xclip -out -sel clip'
+alias yt='yt-dlp -i --add-metadata'
 
 alias pie='perl -p -i -e' # Useful for running substitute commands on files in dir.
 # example: pie 's/replace-text/with-this-text/g' ./*.txt
@@ -43,6 +49,6 @@ cs() { cd "$@" && ls -laF; }
 mkcd() { mkdir -p "$@" && cd "$@"; }
 vic() { vim $(which "$1"); } # Useful for editing scripts on path variable
 
-export clear="[3J[H[2J" # Optimised clear function, clears the screen 5 times faster
-clear() { echo -n $clear; }
+#export clear="[3J[H[2J" # Optimised clear function, clears the screen 5 times faster
+#clear() { echo -n $clear; }
 
