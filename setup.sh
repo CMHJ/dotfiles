c0fbdd566f9ff822c5b2f4ffdaa5a598332cb564
CMHJ <55470933+CMHJ@users.noreply.github.com>
2022-01-25 13:15:57 +1100

    Syncing vscodium settings now

--- setup.sh
+++ setup.sh
@@ -2,15 +2,17 @@
 script_dir="$(dirname "$(readlink -f "$0")")"
 
 # Link configurations to config folder
-for file in "$script_dir"/.config/*; do
-    ln -sf "$file" "${XDG_CONFIG_HOME:-$HOME/.config}"
-    ls -l "${XDG_CONFIG_HOME:-$HOME/.config}"/"$(basename "$file")"
-done
+#for file in "$script_dir"/.config/*; do
+    #ln -sf "$file" "${XDG_CONFIG_HOME:-$HOME/.config}"
+    #ls -l "${XDG_CONFIG_HOME:-$HOME/.config}"/"$(basename "$file")"
+#done
 
-# Apparently an exception must be made because flake8 doesn't follow config
-# spec
 ln -sf "$script_dir"/.config/flake8 "$HOME"/.flake8
-ls -l "$HOME"/.flake8
+ls -l "$HOME"/.config/flake8
+
+ln -sf "$script_dir"/.config/VSCodium/User/settings.json \
+    "${XDG_CONFIG_HOME:-$HOME/.config}"/VSCodium/User/settings.json
+ls -l "${XDG_CONFIG_HOME:-$HOME/.config}"/VSCodium/User/settings.json
 
 #ln -sf "$script_dir"/vimrc $HOME/vimrc
 #ls -l $HOME/vimrc
@@ -20,7 +22,7 @@ ln -sf "$script_dir"/init.vim "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim/init.vim
 ls -l "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim/init.vim
 
 ln -sf "$script_dir"/bash_config.bash "$HOME"/.bash_aliases
-ls -l "$HOME"/bash_aliases
+ls -l "$HOME"/.bash_aliases
 
 # Setup lynx config
 "$script_dir"/lynx/setup
