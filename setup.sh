97a26bf615cc5ae3862efcc69a85b5316b83a469
CMHJ <55470933+CMHJ@users.noreply.github.com>
2022-01-27 20:18:45 +1100

    Fixed flake8 issues

--- setup.sh
+++ setup.sh
@@ -1,21 +1,10 @@
 #!/bin/sh
 script_dir="$(dirname "$(readlink -f "$0")")"
 
-# Link configurations to config folder
-#for file in "$script_dir"/.config/*; do
-    #ln -sf "$file" "${XDG_CONFIG_HOME:-$HOME/.config}"
-    #ls -l "${XDG_CONFIG_HOME:-$HOME/.config}"/"$(basename "$file")"
-#done
-
-ln -sf "$script_dir"/.config/flake8 "$HOME"/.flake8
-ls -l "$HOME"/.config/flake8
-
+mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}"/VSCodium/User/
 ln -sf "$script_dir"/.config/VSCodium/User/settings.json \
     "${XDG_CONFIG_HOME:-$HOME/.config}"/VSCodium/User/settings.json
 ls -l "${XDG_CONFIG_HOME:-$HOME/.config}"/VSCodium/User/settings.json
-
-#ln -sf "$script_dir"/vimrc $HOME/vimrc
-#ls -l $HOME/vimrc
 
 mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim/
 ln -sf "$script_dir"/init.vim "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim/init.vim
