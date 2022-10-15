cde7dfe9f3de6bc522825139c982168e0fb3f56c
CMHJ <55470933+CMHJ@users.noreply.github.com>
2022-10-15 15:40:54 +1100

    added search in address bar back

--- user-overrides.js
+++ user-overrides.js
@@ -1,3 +1,4 @@
 user_pref("browser.startup.page", 3); // Start firefox with last session
 user_pref("privacy.clearOnShutdown.history", false);
+user_pref("keyword.enabled", true); // Enable web search in address bar
 
