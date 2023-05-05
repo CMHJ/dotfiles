103638a9bb3eea5fd9bc05b4608819044ceb6194
CMHJ <55470933+CMHJ@users.noreply.github.com>
2023-05-06 09:12:51 +1000

    added new firefox overrides

--- setup.sh
+++ setup.sh
@@ -27,3 +27,11 @@ ls -l "${XDG_CONFIG_HOME:-$HOME/.config}"/VSCodium/User/settings.json
 # Git
 ln -sf "$script_dir"/gitconfig "$HOME"/.gitconfig
 
+# Firefox
+browserdir="$HOME/.mozilla/firefox"
+profilesini="$browserdir/profiles.ini"
+profile="$(sed -n "/Default=.*.default-release/ s/.*=//p" "$profilesini")"
+pdir="$browserdir/$profile"
+ln -sf "$script_dir/user-overrides.js" "$pdir/user-overrides.js"
+ls -l "$pdir/user-overrides.js"
+
--- user-overrides.js
+++ user-overrides.js
@@ -1,4 +1,45 @@
-user_pref("browser.startup.page", 3); // Start firefox with last session
+// Dont clear history
 user_pref("privacy.clearOnShutdown.history", false);
-user_pref("keyword.enabled", true); // Enable web search in address bar
+
+// Start firefox with last session, requires history not be cleared
+user_pref("browser.startup.page", 3); 
+
+// Enable web search in address bar
+user_pref("keyword.enabled", true);
+
+// Disable the Twitter/Reddit/Facebook ads in the URL bar:
+user_pref("browser.urlbar.quicksuggest.enabled", false);
+user_pref("browser.urlbar.suggest.topsites", false);
+
+// Do not prefil forms:
+user_pref("signon.prefillForms", false);
+
+// Do not autocomplete in the URL bar:
+user_pref("browser.urlbar.autoFill", false);
+
+// Allow access to http (i.e. not https) sites:
+//user_pref("dom.security.https_only_mode", false);
+
+// Keep cookies until expiration or user deletion:
+// user_pref("network.cookie.lifetimePolicy", 0);
+
+
+// Disable push notifications:
+//user_pref("dom.webnotifications.serviceworker.enabled", false);
+//user_pref("dom.push.enabled", false);
+
+// Disable Firefox sync and its menu entries
+user_pref("identity.fxaccounts.enabled", false);
+
+// Disable the pocket antifeature:
+user_pref("extensions.pocket.enabled", false);
+
+// Don't autodelete cookies on shutdown:
+//user_pref("privacy.clearOnShutdown.cookies", false);
+
+// Enable custom userChrome.js:
+//user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// This could otherwise cause some issues on bank logins and other annoying sites:
+//user_pref("network.http.referer.XOriginPolicy", 0);
 
