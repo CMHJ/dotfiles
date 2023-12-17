e1bc9c2cbe1d3ca0de5586e6f7d52d31f2337849
CMHJ <55470933+CMHJ@users.noreply.github.com>
2023-12-17 11:30:04 +1100

    cleaned up setup code

--- new-setup.sh
+++ new-setup.sh
@@ -1,20 +1,16 @@
 #!/bin/sh
 
+# create all config dirs if they don't exist
 dirs="$(find home -type d)"
-# dirs=${dirs#home} # remove first home
 dirs="$(echo $dirs | sed "s|home|$HOME|g")"
-
-# create all paths if they don't exist
 for p in $dirs; do
-    echo $p
     mkdir -p "$p"
 done
 
+# symlink all config files to the appropriate spot in the home dir
 files="$(find home -type f)"
-link_files=
 for f in $files; do
     real_path="$(realpath "$f")"
     link_path="$(echo $f | sed "s|home|$HOME|g")"
-    echo "$real_path" "$link_path"
     ln -sf "$real_path" "$link_path"
 done
