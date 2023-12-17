bd8acec03c2911e3fc0427b198131c4d64924919
CMHJ <55470933+CMHJ@users.noreply.github.com>
2023-12-17 11:52:35 +1100

    added bash config to setup script

--- new-setup.sh
+++ new-setup.sh
@@ -14,3 +14,12 @@ for f in $files; do
     link_path="$(echo $f | sed "s|home|$HOME|g")"
     ln -sf "$real_path" "$link_path"
 done
+
+# point bashrc to bash config in .config
+bash_config_path="$(find home -name config.bash)"
+bash_config_link_path="$(echo $bash_config_path | sed 's|home|\$HOME|g')"
+cat > "$HOME/.bashrc" << EOF
+#!/bin/bash
+
+[ -f "$bash_config_link_path" ] && source "$bash_config_link_path"
+EOF
