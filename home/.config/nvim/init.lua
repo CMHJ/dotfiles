805a448717fe193e94f5664a7a3302ef0383796e
CMHJ <55470933+CMHJ@users.noreply.github.com>
2025-07-31 21:18:22 +1000

    nvim: Changed telescope bindings.

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -164,23 +164,22 @@ vim.keymap.set("n", "<leader>/", function()
     previewer = false,
   })
 end, { desc = "[/] Fuzzily search in current buffer" })
-vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
-vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
-vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
+vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
+vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
+vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
 -- find files in neovim config
-vim.keymap.set("n", "<leader>fn", function()
+vim.keymap.set("n", "<leader>sn", function()
   require("telescope.builtin").find_files {
     cwd = "~/Repos/dotfiles/home/.config/nvim",
   }
-end)
-
-vim.keymap.set("n", "<leader>fw", require("telescope.builtin").grep_string, { desc = "[F]ind Current [W]ord" })
-vim.keymap.set("n", "<leader>fk", require("telescope.builtin").keymaps, { desc = "[F]ind [K]eymaps" })
-vim.keymap.set("n", "<leader>ft", require("telescope.builtin").builtin, { desc = "[F]ind [T]elescope builtin functions" })
-vim.keymap.set("n", "<leader>fd", require("telescope.builtin").diagnostics, { desc = "[F]ind [D]iagnostics" })
-vim.keymap.set("n", "<leader>fr", require("telescope.builtin").resume, { desc = "[F]ind [R]esume" })
-vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers, { desc = "[F]ind [B]uffers"})
-vim.keymap.set("n", "<leader>f.", require("telescope.builtin").oldfiles, { desc = "[F]ind Recent Files ('.' for repeat)" })
+end, { desc = "[S]earch [N]eovim config" })
+vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch Current [W]ord" })
+vim.keymap.set("n", "<leader>sk", require("telescope.builtin").keymaps, { desc = "[S]earch [K]eymaps" })
+vim.keymap.set("n", "<leader>st", require("telescope.builtin").builtin, { desc = "[S]earch [T]elescope builtin functions" })
+vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
+vim.keymap.set("n", "<leader>sr", require("telescope.builtin").resume, { desc = "[S]earch [R]esume" })
+vim.keymap.set("n", "<leader>sb", require("telescope.builtin").buffers, { desc = "[S]earch [B]uffers"})
+vim.keymap.set("n", "<leader>s.", require("telescope.builtin").oldfiles, { desc = "[S]earch Recent Files ('.' for repeat)" })
 
 -- Quickfix list bindings
 vim.keymap.set("n", "J", "<cmd>cnext<CR>")
