1343babfe4a694f4560b461a533e351127cfe58d
CMHJ <55470933+CMHJ@users.noreply.github.com>
2026-05-06 09:34:20 +1000

    nvim: Fixed more issues with treesitter version not working.

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -421,16 +421,25 @@ require("lazy").setup({
       branch = "main",
       lazy = false,
       build = ":TSUpdate",
-      config = function()
-        require("nvim-treesitter.configs").setup({
-          modules = {},
-          ignore_install = {},
-          sync_install = false,
-          auto_install = true,
-          ensure_installed = { "vim", "vimdoc", "lua", "bash", "c", "cpp", "go", "rust", "python", "markdown", "toml", "json" },
-          highlight = { enable = true },
-        })
-      end
+      opts = {
+        modules = {},
+        ignore_install = {},
+        sync_install = false,
+        auto_install = true,
+      },
+      init = function()
+        vim.api.nvim_create_autocmd('FileType', { callback = function()
+            pcall(vim.treesitter.start) -- Enable treesitter highlighting and disable regex syntax
+            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()" -- Enable treesitter-based indentation
+          end, })
+
+        local ensure_installed = { "vim", "vimdoc", "lua", "bash", "c", "cpp", "go", "rust", "python", "markdown", "toml", "json" }
+        local already_installed = require('nvim-treesitter.config').get_installed()
+        local parsers_to_install = vim.iter(ensure_installed)
+          :filter(function(parser) return not vim.tbl_contains(already_installed, parser) end)
+          :totable()
+        require('nvim-treesitter').install(parsers_to_install)
+      end,
     },
     {
       "nvim-telescope/telescope.nvim",
